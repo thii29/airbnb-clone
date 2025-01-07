9cee61aa28f25c66c4d12d7fde31c30c8ca293c7
Thi Pham <thipham1123@gmail.com>
2025-01-07 14:26:01 +0700

    rent catgory list

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { useMemo, useState } from 'react';
+import { FieldValues, useForm } from 'react-hook-form';
 import Heading from '../Heading';
 import useRentModal from '../hooks/useRentModal';
+import CategoryInput from '../inputs/CategoryInput';
 import { categories } from '../navbar/Categories';
 import Modal from './Modal';
 enum STEPS {
@@ -16,7 +18,35 @@ enum STEPS {
 const RentModal = () => {
   const rentModal = useRentModal();
   const [step, setStep] = useState(STEPS.CATEGORY);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: '',
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: '',
+      price: 1,
+      title: '',
+      description: '',
+    },
+  });
+  const category = watch('category');
 
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
   const onBack = () => {
     setStep((value) => value - 1);
   };
@@ -45,7 +75,12 @@ const RentModal = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
         {categories.map((item) => (
           <div key={item.label} className="col-span-1">
-            {item.label}
+            <CategoryInput
+              onClick={(category) => setCustomValue('category', category)}
+              selected={category === item.label}
+              label={item.label}
+              icon={item.icon}
+            />
           </div>
         ))}
       </div>
