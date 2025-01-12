465ea8e6996a1db3d7c58f3bda423c986d416c1d
Thi Pham <thipham1123@gmail.com>
2025-01-12 20:48:07 +0700

    show full listings

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ React-Hook-Form
 React-Hot-Toast
 Mongodb
 Prisma
-NextAuth.js - provide adapter and client for login/sign up with Google, Github, Tweeter, ...
+NextAuth.js - provide adapter and client for login/sign up with Google Github, Tweeter, ...
+Leaflet
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,33 @@
+import getCurrentUser from './actions/getCurrentUser';
 import getListings from './actions/getListings';
 import ClientOnly from './components/ClientOnly';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
+import ListingCard from './components/ListingCard';
 
 export default async function Home() {
-  const listings = await getListings()
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
   const isEmpty = true;
-  if(listings.length === 0){
-    return(
+  if (listings.length === 0) {
+    return (
       <ClientOnly>
-        <EmptyState showReset/>
+        <EmptyState showReset />
       </ClientOnly>
-    )
+    );
   }
   return (
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing: any)=> {
-            return (<div> {listing.title}</div>)
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                key={listing.id}
+                currentUser={currentUser}
+                data={listing}
+              />
+            );
           })}
         </div>
       </Container>
