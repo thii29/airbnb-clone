88270b8d3d206e5357265eda7c9ce36cdbfcf955
Thi Pham <thipham1123@gmail.com>
2025-01-14 20:09:40 +0700

    fix type listing

--- app/components/ListingCard.tsx
+++ app/components/ListingCard.tsx
@@ -1,16 +1,16 @@
 'use client';
-import { Listing, Reservation } from '@prisma/client';
+import { Reservation } from '@prisma/client';
 import { format } from 'date-fns';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useCallback, useMemo } from 'react';
-import { SafeUser } from '../types';
+import { SafeListing, SafeUser } from '../types';
 import Button from './Button';
 import HeartButton from './HeartButton';
 import useCountries from './hooks/useCountries';
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
