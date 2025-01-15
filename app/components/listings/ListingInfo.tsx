82232eb21c9bdf982fcdcd01e0f774486786d6a6
Thi Pham <thipham1123@gmail.com>
2025-01-15 21:58:14 +0700

    finish detail/individual location

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -1,8 +1,15 @@
 'use client';
 
+import ListingCategory from '@/app/components/listings/ListingCategory';
 import { SafeUser } from '@/app/types';
+import dynamic from 'next/dynamic';
 import { IconType } from 'react-icons';
+import Avatar from '../Avatar';
 import useCountries from '../hooks/useCountries';
+
+const Map = dynamic(() => import('../Map'), {
+  ssr: false,
+});
 
 interface ListingInfoProps {
   user: SafeUser;
@@ -36,8 +43,26 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
       <div className="flex flex-col gap-2">
         <div className="text-xl font-semibold flex flex-row items-center gap-2">
           <div>Hosted by {user?.name}</div>
+          <Avatar src={user?.image} />
+        </div>
+        <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
+          <div>{guestCount} guests</div>
+          <div>{roomCount} rooms</div>
+          <div>{bathroomCount} rooms</div>
         </div>
       </div>
+      <hr />
+      {category && (
+        <ListingCategory
+          icon={category.icon}
+          label={category.label}
+          description={category.description}
+        />
+      )}
+      <hr />
+      <div className="text-lg fon-light text-neutral-500">{description}</div>
+      <hr />
+      <Map center={coordinates} />
     </div>
   );
 };
